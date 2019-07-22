c4b2fa7f83eb5dc051b40a6bc577321a1e153646
root <root@localhost.localdomain>
2019-07-22 16:20:50 +0900

    notify

--- onnotify.cpp
+++ onnotify.cpp
@@ -1,71 +1,57 @@
 /* 각 계정마다 누적 금액을 저장할 수 있도록
-/#include <eosio/eosio.hpp>
-#include <eosio/asset.hpp>
+#include<eosio/eosio.hpp>
+#include<eosio/asset.hpp>
 
 using namespace eosio;
 
 CONTRACT onnotify: public contract {
-    public:
-    using contract::contract;
+public:
+using contract::contract;
 
-    ACTION dummy() {}
+ACTION dummy() {}
 
-    [[eosio::on_notify("eosio.token::transfer")]]
-        void ontransfer(name from, name to, asset quantity, std::string memo) {
+[[eosio::on_notify("eosio.token::transfer")]]
+void ontransfer(name from, name to, asset quantity, std::string memo) {
+if(from == get_self()) {
+Receiver receiver(get_self(), get_self().value);
+auto itr = receiver.find(to.value);
+if(itr == receiver.end()) {
+receiver.emplace(from, [&](auto& row) {
+row.user = to;
+row.balance = quantity;
+});
+} else {
+receiver.modify(itr, from, [&](auto& row) {
+row.balance += quantity;
+});
+}
+} else {
+Sender sender(get_self(), get_self().value);
+auto itr = sender.find(from.value);
+if(itr == sender.end()) {
+sender.emplace(to, [&](auto& row) {
+row.user = from;
+row.balance = quantity;
+});
+} else {
+sender.modify(itr, to, [&](auto& row) {
+row.balance += quantity;
+});
+}
+}
+}
+private:
+TABLE trxlist {
+name user;
+asset balance;
 
-        if(from == get_self()) {
-            outs outerTable(get_self(), get_self().value);
-
-            auto itr = outerTable.find(to.value);
-            if(itr == outerTable.end()) {
-                outerTable.emplace(to, [&](auto& row) {
-                    row.outer = to;
-                    row.outbalance = quantity;
-                });
-            } else {
-                outerTable.modify(itr, from, [&](auto& row) {
-                    row.outbalance += quantity;
-                });
-            }
-        } else if(to == get_self()) {
-            inputs inerTable(get_self(), get_self().value);
-
-            auto itr = inerTable.find(to.value);
-            if(itr== inerTable.end()) {
-                inerTable.emplace(from, [&](auto& row) {
-                    row.iner = to;
-                    row.inbalance = quantity;
-                });
-            } else {
-                auto itr = inerTable.begin();
-                inerTable.modify(itr, from, [&](auto& row) {
-                    row.iner = to;
-                    row.inbalance += quantity;
-                });
-            }
-        }
-    }
-    */
-
-/* 
-
-    private:
-        TABLE outstruct {
-            asset outbalance;
-            name outer;
-
-            uint64_t primary_key() const { return outer.value; }
-        };
-
-        TABLE instruct {
-            asset inbalance;
-            name iner;
-
-            uint64_t primary_key() const { return iner.value; }
-        };
-    typedef multi_index<"outss"_n, outstruct> outs;
-    typedef multi_index<"inss"_n, instruct> inputs;
+uint64_t primary_key() const { return user.value; }
 };
+
+typedef multi_index<"eossender"_n, trxlist> Sender;
+typedef multi_index<"eosreceiver"_n, trxlist> Receiver;
+};
+*/
 
 /* 입금 내역 조회(in)
 #include <eosio/eosio.hpp>
